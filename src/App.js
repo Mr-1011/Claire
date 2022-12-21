9888dbf59ed7d51c606146bf09c325b22edc89ad
thatkidfromka <93349629+thatkidfromka@users.noreply.github.com>
2022-12-21 17:23:47 +0100

    import speedometer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Speedometer from "./components/speedometer/speedometer";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={
-            <h1>start</h1>
-          } />
+          <Route path='/' element={<Speedometer/> } />
           <Route path='/question' element={
             <h1>question</h1>
           } />
