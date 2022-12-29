768ac7d4eeb8b7d8372a7e44173c2fdcf198ac71
thatkidfromka <93349629+thatkidfromka@users.noreply.github.com>
2022-12-29 16:00:18 +0100

    comment

--- src/components/speedometer/speedometer.jsx
+++ src/components/speedometer/speedometer.jsx
@@ -6,20 +6,39 @@ import { useState } from "react";
 
 const Speedometer = () =>{
 
+// set the start scenario: startspeed, acceleration (function or constant) and delta t (time horizon predicition)
 let acceleration = 10;
 const startspeed = 100;
 let predicition_horizon = 5;
 
-const sleep = ms => new Promise(r => setTimeout(r, ms));
-
+// angles for the needles in the speedometer(alpha = currentspeed)
 const [alpha,setAlpha] = useState(100);
 const [beta,setBeta] = useState(90);
 
-const setSpeed = (acceleration,startspeed,predicition_horizon) => {
-	let speed = startspeed+predicition_horizon*acceleration;
+//calculate the speed continiously and update the needle
+//speed(t) = startspeed+t*acceleration
+
+const setSpeed = () =>{
+	console.log("button pressed");
+	var startDate = new Date();
+	var currentDate = new Date();
+	while(Math.abs(currentDate-startDate)<100000){
+		currentDate = new Date();
+		var delta_t = Math.abs(currentDate-startDate)/1000;
+		// formula for updating the speed: 
+		var speed = startspeed + acceleration*delta_t;
+		setAlpha(speed);
+	}
+
+}
+//predicted_speed = speed * Time_horizon* acceleration
+const setPred_Speed = (acceleration,startspeed,predicition_horizon) => {
+	let speed = speed+predicition_horizon*acceleration;
 	return speed;
 }
-	
+
+
+
     return(
         <div className="engine">
         <div className="head">
@@ -30,15 +49,15 @@ const setSpeed = (acceleration,startspeed,predicition_horizon) => {
 	        <div className="meter meter--rpm meter--big-label"></div>
 	        <div className="meter meter--gear"><div>1</div></div>
 	        <div className="meter meter--speed">
-				<div className="needle" style={{transform: "rotate("+startspeed+"deg)"}}></div>
-				<div className="needle" style={{background:"#00FF00",transform: "rotate("+setSpeed(acceleration,startspeed,predicition_horizon)+"deg)"}}></div>
+				<div className="needle" style={{transform: "rotate("+alpha+"deg)"}}></div>
+				<div className="needle" style={{background:"#00FF00",transform: "rotate("+setPred_Speed(acceleration,startspeed,predicition_horizon)+"deg)"}}></div>
 			</div>
 			
         </div>
 
        
 
-        <button className="btn-volume active">♩</button>
+        <button className="btn-volume active" onClick={setSpeed}>start</button>
 
         
         </div>
