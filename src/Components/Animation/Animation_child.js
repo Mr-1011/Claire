9dfe554653deb073daea6523a44a361a90d499fe
thatkidfromka <93349629+thatkidfromka@users.noreply.github.com>
2023-01-14 13:56:57 +0100

    fixed size

--- src/Components/Animation/Animation_child.js
+++ src/Components/Animation/Animation_child.js
@@ -1,18 +1,14 @@
-import { useState} from "react";
-import Grid from '@mui/material/Grid';
+import { useState } from "react";
+import Grid from "@mui/material/Grid";
 import "./animation.css";
-import {
-  Dashboard,
-  DashboardWrapper,
-  RoadWrapper,
-} from "./Dashboard";
+import { Dashboard, DashboardWrapper, RoadWrapper } from "./Dashboard";
 import { Road, TrafficWrapper } from "./Road";
 import { Car_Img, Child } from "../../assets/exports";
 import Speedometer_child from "../speedometer/speedometer_child";
 import Batterystatus from "../speedometer/batterystatus";
 import React from "react";
-import VibrationOutlinedIcon from '@mui/icons-material/VibrationOutlined';
-import AirlineSeatReclineExtraOutlinedIcon from '@mui/icons-material/AirlineSeatReclineExtraOutlined';
+import VibrationOutlinedIcon from "@mui/icons-material/VibrationOutlined";
+import AirlineSeatReclineExtraOutlinedIcon from "@mui/icons-material/AirlineSeatReclineExtraOutlined";
 
 //code from sirarifarid-fiverr
 //showTraffic and stopRoad useStates define behavior of animation. (traffic light appears and car stops = road stops)
@@ -41,7 +37,7 @@ function Animation_child() {
   }, []);
   */
   React.useEffect(() => {
-    let timers: any = [];
+    let timers = [];
     const car_running_time = 12000;
     const traffic_delay = 4000;
     const restart_loop_delay = 4000;
@@ -71,36 +67,42 @@ function Animation_child() {
   }, []);
 
   return (
-    
     <Grid container column={2}>
-        <Grid item xs={4}>
-            <Speedometer_child dataFromParent={{child:showChild,stop:stopRoad}} />
-            <VibrationOutlinedIcon style={{height:100,width:100,color:iconcolor}}/>
-        </Grid>
-        <Grid item xs={4}>
-            <Dashboard>
-            <img src={Car_Img} />
-            <DashboardWrapper>
-                {showChild && (
-                <TrafficWrapper className="child">
-                    <img src={Child} />
-                </TrafficWrapper>
-                )}
-                <RoadWrapper className={stopRoad ? "slowroad" : "dashboard_wrapper"}>
-                <Road></Road>
-                <Road></Road>
-                <Road></Road>
-                <Road></Road>
-                </RoadWrapper>
-            </DashboardWrapper>
-            </Dashboard>
-        </Grid>
-        <Grid item xs={4}>
-            <Batterystatus/>
-            <AirlineSeatReclineExtraOutlinedIcon style={{height:100,width:100,color:iconcolor}}/>
-        </Grid>
+      <Grid item xs={4}>
+        <Speedometer_child
+          dataFromParent={{ child: showChild, stop: stopRoad }}
+        />
+        <VibrationOutlinedIcon
+          style={{ height: 100, width: 100, color: iconcolor }}
+        />
+      </Grid>
+      <Grid item xs={4}>
+        <Dashboard>
+          <img src={Car_Img} />
+          <DashboardWrapper>
+            {showChild && (
+              <TrafficWrapper className="child">
+                <img src={Child} />
+              </TrafficWrapper>
+            )}
+            <RoadWrapper
+              className={stopRoad ? "slowroad" : "dashboard_wrapper"}
+            >
+              <Road></Road>
+              <Road></Road>
+              <Road></Road>
+              <Road></Road>
+            </RoadWrapper>
+          </DashboardWrapper>
+        </Dashboard>
+      </Grid>
+      <Grid item xs={4}>
+        <Batterystatus />
+        <AirlineSeatReclineExtraOutlinedIcon
+          style={{ height: 100, width: 100, color: iconcolor }}
+        />
+      </Grid>
     </Grid>
-    
   );
 }
 
--- src/components/Animation/Animation.js
+++ src/components/Animation/Animation.js
@@ -1,20 +1,17 @@
-import { useState} from "react";
-import Grid from '@mui/material/Grid';
+import { useState } from "react";
+import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 import "./animation.css";
-import {
-  Dashboard,
-  DashboardWrapper,
-  RoadWrapper,
-} from "./Dashboard";
+import { Dashboard, DashboardWrapper, RoadWrapper } from "./Dashboard";
 import { Road, TrafficWrapper } from "./Road";
 import { Car_Img, Traffic } from "../../assets/exports";
 import Speedometer from "../speedometer/speedometer";
 //import Speedometer from "../testfield/Speedometer";
 import Batterystatus from "../speedometer/batterystatus";
 import React from "react";
-import VibrationOutlinedIcon from '@mui/icons-material/VibrationOutlined';
-import AirlineSeatReclineExtraOutlinedIcon from '@mui/icons-material/AirlineSeatReclineExtraOutlined';
+import VibrationOutlinedIcon from "@mui/icons-material/VibrationOutlined";
+import AirlineSeatReclineExtraOutlinedIcon from "@mui/icons-material/AirlineSeatReclineExtraOutlined";
+import { Box, Stack } from "@mui/material";
 
 //code from sirarifarid-fiverr
 //showTraffic and stopRoad useStates define behavior of animation. (traffic light appears and car stops = road stops)
@@ -23,7 +20,7 @@ function Animation() {
   const [showTraffic, setShowTraffic] = useState(false);
   const [stopRoad, setStopRoad] = useState(false);
   const [iconcolor, seticonColor] = useState("green");
-  
+
   /*
   React.useEffect(() => {
     const showTraffic = setTimeout(() => {
@@ -39,9 +36,9 @@ function Animation() {
   }, []);
   
 */
-  
+
   React.useEffect(() => {
-    let timers: any = [];
+    let timers = [];
     const car_running_time = 12000;
     const traffic_delay = 4000;
     const restart_loop_delay = 4000;
@@ -70,53 +67,89 @@ function Animation() {
     };
   }, []);
 
-
-
   return (
-    
-    <Grid container column={3}>
-        <Grid item xs={4}>
-            <Speedometer dataFromParent={{traffic:showTraffic,stop:stopRoad}} />
-            <VibrationOutlinedIcon style={{height:100,width:100,color:iconcolor}}/>
+    <Stack
+      sx={{
+        overflow: "hidden",
+        maxWidth: 1100,
+        margin: "auto",
+        height: "100vh",
+        alignItems: "center",
+      }}
+      flexDirection={"row"}
+      justifyContent={"space-between"}
+    >
+      <Box
+        sx={{
+          width: "336px",
+          height: "600px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          scale: "0.6",
+        }}
+      >
+        <Speedometer
+          dataFromParent={{ traffic: showTraffic, stop: stopRoad }}
+        />
+        <VibrationOutlinedIcon
+          style={{ height: 100, width: 100, color: iconcolor }}
+        />
+      </Box>
+      <Box
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Dashboard>
+          <img src={Car_Img} />
+          <DashboardWrapper>
+            {showTraffic && (
+              <TrafficWrapper className="traffic">
+                <img src={Traffic} />
+              </TrafficWrapper>
+            )}
+            <RoadWrapper className={stopRoad ? "" : "dashboard_wrapper"}>
+              <Road></Road>
+              <Road></Road>
+              <Road></Road>
+              <Road></Road>
+            </RoadWrapper>
+          </DashboardWrapper>
+        </Dashboard>
+      </Box>
+      <Box
+        sx={{
+          width: "336px",
+          scale: "0.6",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Batterystatus />
+        <Grid container direction="column">
+          <Grid item>
+            <AirlineSeatReclineExtraOutlinedIcon
+              style={{ height: 100, width: 100, color: iconcolor }}
+            />
+          </Grid>
+          <Grid item>
+            <Link to="/speedometer_child">
+              <button
+                style={{
+                  color: "white",
+                  background: "black",
+                  borderColor: "black",
+                }}
+              >
+                next scenario
+              </button>
+            </Link>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-            
-            <Dashboard>
-            <img src={Car_Img} />
-            <DashboardWrapper>
-                {showTraffic && (
-                <TrafficWrapper className="traffic">
-                    <img src={Traffic} />
-                </TrafficWrapper>
-                )}
-                <RoadWrapper className={stopRoad ? "" : "dashboard_wrapper"}>
-                <Road></Road>
-                <Road></Road>
-                <Road></Road>
-                <Road></Road>
-                </RoadWrapper>
-            </DashboardWrapper>
-            </Dashboard>
-          
-        </Grid>
-        <Grid item xs={4}>
-            <Batterystatus/>
-              <Grid container direction="column">
-                <Grid item>
-                <AirlineSeatReclineExtraOutlinedIcon style={{height:100,width:100,color:iconcolor}}/>
-                </Grid>
-                <Grid item>
-                <Link to='/speedometer_child'>
-                  <button style={{color:"white",background:"black",borderColor:"black"}}>
-                    next scenario
-                  </button>
-                </Link>
-                </Grid>
-                </Grid>
-        </Grid>
-           
-      </Grid>
-    
+      </Box>
+    </Stack>
   );
 }
 
